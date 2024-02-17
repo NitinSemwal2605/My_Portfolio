b0ed0abb376f7045c86def697f6bc82e76ebd43e
NitinSemwal <140317834+NitinSemwal2614@users.noreply.github.com>
2024-02-17 12:32:52 +0530

    New Code

---
+++ src/components/Testimonials/Data.jsx
@@ -0,0 +1,25 @@
+import { default as Image1} from "../../assets/testimonial1.png";
+import { default as Image2} from "../../assets/testimonial2.png";
+import { default as Image3} from "../../assets/testimonial3.png";
+// , default as Image2, default as Image3 
+
+export const Data = [
+    {
+        id:1,
+        image:Image1,
+        title:"Tejas Goyal",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptates."
+    },
+    {
+        id:2,
+        image:Image2,
+        title:"Bhaskar Semwal",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptates."
+    },
+    {
+        id:3,
+        image:Image3,
+        title:"Dhruv Naruka",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptates."
+    },
+];
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className='home_social'>
+        <a href="https://www.facebook.com/profile.php?id=61554799427814" className="home_social-icon" target='_blank' rel='noopener noreferrer'>
+            <i class="uil uil-facebook"></i>
+        </a>
+
+        <a href="https://twitter.com/NitinSem05" className="home_social-icon" target='_blank' rel='noopener noreferrer'>
+            <i class="uil uil-twitter-alt"></i>
+        </a>
+
+        <a href="https://www.instagram.com/nitinsemwal26?igsh=OW1qN3NqeGc2dm9u" className="home_social-icon" target='_blank' rel='noopener noreferrer'>
+            <i className="uil uil-instagram"></i>
+        </a>
+        <a href="https://github.com/NitinSemwal2614" className="home_social-icon" target='_blank' rel='noopener noreferrer'>
+            <i class="uil uil-github"></i>
+        </a>
+
+
+    </div>
+  );
+};
+
+export default Social;
